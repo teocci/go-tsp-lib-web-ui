c4d28f16dde5a0312aec30328c4c9ad3e608235e
amissu89 <yonglimlee@gmail.com>
2022-05-16 16:15:19 +0900

    changed step lines color and style

--- web/static/js/constant.js
+++ web/static/js/constant.js
@@ -18,7 +18,7 @@ const MARKERS = {
 /* color */
 const TLIB_ROUTE_COLOR = '#e04010'
 const TMAP_ROUTE_COLOR = '#007196'
-const PARTLY_ROUTE_COLOR = '#eaed1a'
+const PARTLY_ROUTE_COLOR = '#00FF00'
 
 const TLIB_ROUTE_POLYLINE = {
     endArrow: true, strokeWeight: 4, // 선의 두께 입니다
@@ -52,7 +52,7 @@ const PARTLY_POLYLINE = {
     endArrow: true, strokeWeight: 6, // 선의 두께 입니다
     strokeColor: PARTLY_ROUTE_COLOR, // 빨간색
     strokeOpacity: 0.8, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-    strokeStyle: 'dash' // 선의 스타일입니다
+    strokeStyle: 'shortdot' // 선의 스타일입니다
 }
 
 const POLYLINE_TYPE_ROUTE = 'route'
